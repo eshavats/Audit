3736ff18cd5c96398127b435b997a84542a670f4
Esha Vats <56391290+eshavats@users.noreply.github.com>
2020-07-27 02:10:45 +0530

    enable cookies

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,14 @@ const mongoose = require("mongoose");
 
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => done(null, user));
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -13,7 +21,17 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // we already have a record with the given profile ID
+          done(null, existingUser);
+        } else {
+          //we don't have a record with the given profile ID, make a new record
+          new User({ googleId: profile.id })
+            .save()
+            .then((user) => done(null, user));
+        }
+      });
     }
   )
 );
