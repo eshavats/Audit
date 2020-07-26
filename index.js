8daa4ba86c7d8cc34e74a421a14615a3797af00f
Esha Vats <56391290+eshavats@users.noreply.github.com>
2020-07-26 22:45:57 +0530

    add google oauth

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 const app = express();
-const port = process.env.PORT || 5000;
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("Access Token", accessToken);
+      console.log("Refresh Token", refreshToken);
+      console.log("Profile", profile);
+    }
+  )
+);
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
