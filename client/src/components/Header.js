fd0fb665985a3918fdcb4c3f1df42e5328382965
Esha Vats <56391290+eshavats@users.noreply.github.com>
2020-08-04 01:56:21 +0530

    create landing component and redirect on logo click

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends React.Component {
   renderContent() {
@@ -29,9 +30,9 @@ class Header extends React.Component {
         <div className="nav-wrapper">
           <div className="row">
             <div className="col s6">
-              <a href="#" className="left brand-logo">
+              <Link to={this.props.auth ? "/surveys" : "/"} className="left brand-logo">
                 Audit
-              </a>
+              </Link>
             </div>
 
             <ul className="right">{this.renderContent()}</ul>
