a28acf5bec291f45874ea75ce065585722ca5181
Esha Vats <56391290+eshavats@users.noreply.github.com>
2020-08-04 01:41:56 +0530

    redirect user on auth and logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -17,7 +17,7 @@ class Header extends React.Component {
       default:
         return (
           <li>
-            <a href="">Logout</a>
+            <a href="/api/logout">Logout</a>
           </li>
         );
     }
