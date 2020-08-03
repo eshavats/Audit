953e43717bc5bcb48f689722b0bbc073b0b70e64
Esha Vats <56391290+eshavats@users.noreply.github.com>
2020-08-03 21:39:49 +0530

    add routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => <div>Header</div>;
+const Landing = () => <div>Landing</div>;
+const Dashboard = () => <div>Dashboard</div>;
+const SurveyNew = () => <div>SurveyNew</div>;
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
