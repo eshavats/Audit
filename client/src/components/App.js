1f04d103608c3e78ecdaff9811eec81cdf858a1f
Esha Vats <56391290+eshavats@users.noreply.github.com>
2020-08-04 01:06:00 +0530

    add state in header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,32 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 import Header from "./Header";
 const Landing = () => <div>Landing</div>;
 const Dashboard = () => <div>Dashboard</div>;
 const SurveyNew = () => <div>SurveyNew</div>;
 
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
 
 class Header extends React.Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+
+      default:
+        return (
+          <li>
+            <a href="">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
@@ -12,11 +34,7 @@ class Header extends React.Component {
               </a>
             </div>
 
-            <ul className="right">
-              <li>
-                <a>Login with Google</a>
-              </li>
-            </ul>
+            <ul className="right">{this.renderContent()}</ul>
           </div>
         </div>
       </nav>
@@ -24,4 +42,8 @@ class Header extends React.Component {
   }
 }
 
-export default Header;
+const mapStateToProps = ({ auth }) => {
+  return { auth };
+};
+
+export default connect(mapStateToProps)(Header);
